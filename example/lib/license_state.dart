f4c833b64e77b7c4ce617929b4a22675fb4ac2c3
Bernd Kampl <97178352+berndkamplanyline@users.noreply.github.com>
2025-07-07 15:51:05 +0200

    Release 55.2.1 (#59)

--- example/lib/license_state.dart
+++ example/lib/license_state.dart
@@ -1,9 +1,9 @@
 class LicenseState {
+
+  LicenseState(this.initialized, this.reason);
   static const LicenseKeyEmptyErrorMessage =
       'Please ensure that your license key is valid and set correctly in .env.\n\nFor more information, please check https://documentation.anyline.com/main-component/license-key-generation.html';
 
   late bool initialized;
   late String reason;
-
-  LicenseState(this.initialized, this.reason);
 }
